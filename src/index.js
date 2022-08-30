c5cbab2384e95e9cb39560e4e996e059e4056fd1
Rasso Hilber <mail@rassohilber.com>
2022-08-30 16:05:41 +0200

    Allow submitting to a new tab in Chrome, Safari & Edge

--- src/index.js
+++ src/index.js
@@ -16,6 +16,14 @@ export default class FormPlugin extends Plugin {
 			...defaultOptions,
 			...options
 		};
+
+		/**
+		 * Helps detecting form submits to a new tab
+		 */
+		this.specialKeys = {
+			Meta: false,
+			Control: false
+		};
 	}
 
 	mount() {
@@ -32,47 +40,54 @@ export default class FormPlugin extends Plugin {
 			'submit',
 			this.onFormSubmit.bind(this)
 		);
+
+		document.addEventListener('keydown', this.onKeyDown);
+		document.addEventListener('keyup', this.onKeyUp);
 	}
 
 	unmount() {
 		const swup = this.swup;
 
 		swup.delegatedListeners.formSubmit.destroy();
+
+		document.removeEventListener('keydown', this.onKeyDown);
+		document.removeEventListener('keyup', this.onKeyUp);
 	}
 
 	onFormSubmit(event) {
 		const swup = this.swup;
 
-		// no control key pressed
-		if (!event.metaKey) {
-			const form = event.target;
-			const data = new FormData(form);
-			const action = form.getAttribute('action') || window.location.href;
-			const method = (form.getAttribute('method') || 'get').toUpperCase();
-			const customTransition = form.getAttribute('data-swup-transition');
+		// Bail early if a special key is pressed
+		if (this.isSpecialKeyPressed()) {
+			this.handleSpecialKeySubmit(event);
+			return;
+		}
 
-			const link = new Link(action);
-			const hash = link.getHash();
-			let url = link.getAddress();
+		const form = event.target;
+		const data = new FormData(form);
+		const action = form.getAttribute('action') || window.location.href;
+		const method = (form.getAttribute('method') || 'get').toUpperCase();
+		const customTransition = form.getAttribute('data-swup-transition');
 
-			swup.triggerEvent('submitForm', event);
+		const link = new Link(action);
+		const hash = link.getHash();
+		let url = link.getAddress();
 
-			event.preventDefault();
+		swup.triggerEvent('submitForm', event);
 
-			if (hash) {
-				swup.scrollToElement = hash;
-			}
+		event.preventDefault();
 
-			if (method === 'GET') {
-				url = this.appendQueryParams(url, data);
-				swup.cache.remove(url);
-				swup.loadPage({ url, customTransition });
-			} else {
-				swup.cache.remove(url);
-				swup.loadPage({ url, method, data, customTransition });
-			}
+		if (hash) {
+			swup.scrollToElement = hash;
+		}
+
+		if (method === 'GET') {
+			url = this.appendQueryParams(url, data);
+			swup.cache.remove(url);
+			swup.loadPage({ url, customTransition });
 		} else {
-			swup.triggerEvent('openFormSubmitInNewTab', event);
+			swup.cache.remove(url);
+			swup.loadPage({ url, method, data, customTransition });
 		}
 	}
 
@@ -80,5 +95,49 @@ export default class FormPlugin extends Plugin {
 		url = url.split('?')[0];
 		const query = new URLSearchParams(formData).toString();
 		return query ? `${url}?${query}` : url;
+	}
+
+	/**
+	 * Is either command or control key down at the moment
+	 * @returns {boolean}
+	 */
+	isSpecialKeyPressed() {
+		return Object.values(this.specialKeys).some((value) => value);
+	}
+
+	/**
+	 * Adjust `specialKeys` on keyDown
+	 * @param {KeyboardEvent} e
+	 * @returns {void}
+	 */
+	onKeyDown = (e) => {
+		if (!this.specialKeys.hasOwnProperty(e.key)) return;
+		this.specialKeys[e.key] = true;
+	};
+
+	/**
+	 * Adjust `specialKeys` on keyUp
+	 * @param {KeyboardEvent} e
+	 * @returns {void}
+	 */
+	onKeyUp = (e) => {
+		if (!this.specialKeys.hasOwnProperty(e.key)) return;
+		this.specialKeys[e.key] = false;
+	};
+
+	/**
+	 * Handles a form being submitted while pressing the command and/or control key.
+	 * Will wait for a `visibilitychange` for one second, and if it happens and the new
+	 * state is 'hidden', fires the event. Will stop to listen after one second.
+	 * @param {SubmitEvent} submitEvent
+	 */
+	handleSpecialKeySubmit(submitEvent) {
+		const onVisibilityChange = () => {
+			if (document.visibilityState === 'hidden') {
+				swup.triggerEvent('openFormSubmitInNewTab', submitEvent);
+			}
+		};
+		window.addEventListener('visibilitychange', onVisibilityChange, { once: true });
+		setTimeout(() => window.removeEventListener('visibilitychange', onVisibilityChange), 1000);
 	}
 }
